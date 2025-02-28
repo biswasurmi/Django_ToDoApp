ea6320070564c01ef186e774610cdf68cf08c699
Urmi Biswas <139246745+biswasurmi@users.noreply.github.com>
2025-02-28 13:16:16 +0600

    Update urls.py

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    path('', auth_views.LoginView.as_view(template_name = 'todoapp/login.html'), name = 'login'),
     path('hello/', views.hello, name = 'hello'),
     path('hello_protected/', views.hello_protected, name = 'hello_protected'),
     path('login/', auth_views.LoginView.as_view(template_name = 'todoapp/login.html'), name = 'login'),
